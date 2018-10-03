ba6483d0a18ec9f391dfe502d158539c2a9f9cdf
Justin Tran <MMusto@users.noreply.github.com>
2018-10-02 17:09:51 -0700

    Bug Fixes/QoL additions and Docstring edits
    cleanup

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ async def on_ready():
 	
 @client.command(pass_context=True)
 async def mah(ctx):
-	'''"Move-All-Here" : Moves all players to your current voice channel '''
+	'''"Move-All-Here" : Moves everyone to your current voice channel '''
 	author = ctx.message.author
 	if author.server_permissions.move_members:
 		for channel in client.get_all_channels():
@@ -28,8 +28,8 @@ async def mah(ctx):
 	else:
 		await client.say("Sorry you don't have permissions for that.")
 		
-#Helper function for mcc
 def get_channel(chname : str) -> "Channel":
+'''Helper function that returns Channel object from name snippet'''
 	for channel in client.get_all_channels():
 		if(str(channel.type) == 'voice'):
 			if(chname.lower() in channel.name.lower()):
@@ -38,11 +38,15 @@ def get_channel(chname : str) -> "Channel":
 		
 @client.command(pass_context=True)
 async def mcc(ctx, chname1 : str, chname2 : str):
-	'''"Move-Channel-to-Channel" : .mcc (CHANNEL 1) (CHANNEL 2) -  Moves all players from first Channel 1 to Channel 2'''
-	ch1 = get_channel(chname1).voice_members
+	'''"Move-Channel-to-Channel" : .mcc (CHANNEL 1) (CHANNEL 2) -  Moves everyone from Channel 1 to Channel 2'''
+	ch1 = get_channel(chname1)
 	ch2 = get_channel(chname2)
-	if ch1 == None or ch2 == None:
-		await client.say("Sorry, one of the channels could not be found.")
+	if ch1 == None and ch2 != None:
+		await client.say("Sorry, '" + chname1 + "' could not be found.")
+	elif ch2 == None and ch1 != None:
+		await client.say("Sorry, '" + chname2 + "' could not be found.")
+	elif ch2 == None and ch1 = None:
+		await client.say("Sorry, both '" + chname1 + "' and  '" + chname2 + "' could not be found.")
 	else:
 		lst = []
 		for member in get_channel(chname1).voice_members:
@@ -50,7 +54,7 @@ async def mcc(ctx, chname1 : str, chname2 : str):
 		for member in lst:
 			await client.move_member(member, ch2)
 @client.command
-async def help():
+async def commands():
 	await client.say('.mah - "Move-All-Here : Moves all players to your current voice channel')
 	await client.say('.mcc - "Move-Channel-To-Channel" : .mcc (CHANNEL 1) (CHANNEL 2)  Moves all players from first channel to second channel')
 
