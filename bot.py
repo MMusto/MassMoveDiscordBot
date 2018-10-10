1d92693a2cbd4ce45d8d2091bf4a96dce8d8d23c
Justin Tran <MMusto@users.noreply.github.com>
2018-10-09 18:00:22 -0700

    Added clean up
    Commands sent should now delete after completion.

--- bot.py
+++ bot.py
@@ -15,24 +15,27 @@ async def mah(ctx) -> None:
 	'''"Move-All-Here" : Moves everyone to your current voice channel '''
 	author = ctx.message.author
 	if author.server_permissions.move_members:
+		#Finds voice channel that author is in
 		for channel in client.get_all_channels():
 			if(str(channel.type) == 'voice' and  author in channel.voice_members):
 					move_channel = channel
 					break
+		#Moves all people to your channel that was found above
 		for member in client.get_all_members():
 			if(member.voice_channel != None and not member.is_afk and member != author):
 				await client.move_member(member, move_channel)
 				await client.say("Mass moved everyone to " + str(move_channel))
+		client.delete_message(ctx.message)
 	else:
 		await client.say("Sorry you don't have permissions for that.")
 		
 def get_channel(chname : str) -> "Channel":
-        '''Helper function that returns Channel object from name snippet'''
-        for channel in client.get_all_channels():
-                if(str(channel.type) == 'voice'):
-                        if(chname.lower() in channel.name.lower()):
-                                return channel
-        return None
+	'''Helper function that returns Channel object from name snippet'''
+	for channel in client.get_all_channels():
+			if(str(channel.type) == 'voice'):
+					if(chname.lower() in channel.name.lower()):
+							return channel
+	return None
 		
 @client.command(pass_context=True)
 async def mcc(ctx, chname1 : str, chname2 : str) -> None:
@@ -49,5 +52,12 @@ async def mcc(ctx, chname1 : str, chname2 : str) -> None:
 		lst = [member for member in ch1.voice_members]
 		for member in lst:
 			await client.move_member(member, ch2)
+		client.delete_message(ctx.message)
+		
+@client.command(pass_context=True)
+asycn def clear(ctx) -> None:
+	'''Clears all error messages from this bot'''
+	pass
+	
 
 client.run(TOKEN)
