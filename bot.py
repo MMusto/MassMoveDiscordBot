a312b4f0a1d2498a4212ae705ac05cff739f0f37
Justin Tran <MMusto@users.noreply.github.com>
2018-11-11 15:27:51 -0800

    Permissions fix, and server indpendency added
    Added permission requirements to functions and added major channel and member iterations specific to server command was executed on, as opposed to iterating through all of the bots servers and getting channels/members.

--- bot.py
+++ bot.py
@@ -13,14 +13,17 @@ async def on_ready():
 async def mah(ctx) -> None:
 	'''"Move-All-Here" : Moves everyone to your current voice channel '''
 	author = ctx.message.author
+	server = author.server
+	all_members = server.members
+	channels = server.channels
 	if author.server_permissions.move_members:
 		#Finds voice channel that author is in
-		for channel in client.get_all_channels():
+		for channel in channels:
 			if(str(channel.type) == 'voice' and  author in channel.voice_members):
 					move_channel = channel
 					break
 		#Moves all people to your channel that was found above
-		for member in client.get_all_members():
+		for member in all_members:
 			if(member.voice_channel != None and not member.is_afk and member != author):
 				await client.move_member(member, move_channel)
 				await client.say("Mass moved everyone to " + str(move_channel))
@@ -28,9 +31,9 @@ async def mah(ctx) -> None:
 	else:
 		await client.say("Sorry you don't have permissions for that.")
 		
-def get_channel(chname : str) -> "Channel":
+def get_channel(server, chname : str) -> "Channel":
 	'''Helper function that returns Channel object from name snippet'''
-	for channel in client.get_all_channels():
+	for channel in server.channels:
 			if(str(channel.type) == 'voice'):
 					if(chname.lower() in channel.name.lower()):
 							return channel
@@ -39,37 +42,48 @@ def get_channel(chname : str) -> "Channel":
 @client.command(pass_context=True)
 async def mcc(ctx, chname1 : str, chname2 : str) -> None:
 	'''"Move-Channel-to-Channel" : .mcc (CHANNEL 1) (CHANNEL 2) -  Moves everyone from Channel 1 to Channel 2'''
-	ch1 = get_channel(chname1)
-	ch2 = get_channel(chname2)
-	if(ch1 == None and ch2 != None):
-		await client.say("Sorry, '" + chname1 + "' could not be found.")
-	elif(ch2 == None and ch1 != None):
-		await client.say("Sorry, '" + chname2 + "' could not be found.")
-	elif(ch2 == None and ch1 == None):
-		await client.say("Sorry, both '" + chname1 + "' and  '" + chname2 + "' could not be found.")
+	if ctx.message.author.server_permissions.move_members:
+		server = ctx.message.author.server
+		ch1 = get_channel(server, chname1)
+		ch2 = get_channel(server, chname2)
+		if(ch1 == None and ch2 != None):
+			await client.say("Sorry, '" + chname1 + "' could not be found.")
+		elif(ch2 == None and ch1 != None):
+			await client.say("Sorry, '" + chname2 + "' could not be found.")
+		elif(ch2 == None and ch1 == None):
+			await client.say("Sorry, both '" + chname1 + "' and  '" + chname2 + "' could not be found.")
+		else:
+			lst = [member for member in ch1.voice_members]
+			for member in lst:
+				await client.move_member(member, ch2)
+		await client.delete_message(ctx.message)
 	else:
-		lst = [member for member in ch1.voice_members]
-		for member in lst:
-			await client.move_member(member, ch2)
+		await client.say("Sorry you don't have permissions for that.")
 		
 @client.command(pass_context=True)
 async def mbr(ctx, role : str, chname : str) -> None:
 	'''"Move-By-Role" : .mbr (ROLE_NAME) (CHANNEL x) -  Moves everyone from with Role ROLE_NAME to Channel x'''
-	ch = get_channel(chname)
-	got_role = get_role(ctx.message.author.server, role)
-			
-	if(ch == None and got_role == None):
-		await client.say("Sorry, both '" + chname + "' and '" + role + "' could not be found.")
-	elif(ch == None):
-		await client.say("Sorry, '" + chname + "' could not be found.")
-	elif(got_role == None):
-		await client.say("Sorry, '" + role + "' could not be found.")
-	for member in client.get_all_members():
-		if(member.voice_channel != None and member.voice_channel != ch and not member.is_afk):
-			for rolee in member.roles:
-				if rolee == got_role:
-					await client.move_member(member, ch)
-					await client.say("Moved Member: " + member.name + " with role " + got_role.name + " to channel " + chname)
+	if ctx.message.author.server_permissions.move_members:
+		server = ctx.message.author.server
+		ch = get_channel(server, chname)
+		got_role = get_role(server, role)
+		all_members = server.members
+				
+		if(ch == None and got_role == None):
+			await client.say("Sorry, both '" + chname + "' and '" + role + "' could not be found.")
+		elif(ch == None):
+			await client.say("Sorry, '" + chname + "' could not be found.")
+		elif(got_role == None):
+			await client.say("Sorry, '" + role + "' could not be found.")
+		for member in all_members:
+			if(member.voice_channel != None and member.voice_channel != ch and not member.is_afk):
+				for rolee in member.roles:
+					if rolee == got_role:
+						await client.move_member(member, ch)
+						await client.say("Moved Member: " + member.name + " with role " + got_role.name + " to channel " + ch.name)
+		await client.delete_message(ctx.message)
+	else:
+		await client.say("Sorry you don't have permissions for that.")
 					
 def get_role(server, role : str) -> 'Role':
 	'''Helper function that returns Role object from name snippet'''
@@ -77,32 +91,18 @@ def get_role(server, role : str) -> 'Role':
 		if role.lower() in roleo.name.lower():
 			return roleo
 	return None
-@client.command(pass_context=True)
-async def clear(ctx,lim=1) -> None:
-	'''Clears all error messages from this bot'''
-	dlist = []
-	async for message in client.logs_from(ctx.message.channel,limit=int(lim)):
-		dlist.append(message)
-	await client.delete_messages(dlist)
+	
+# @client.command(pass_context=True)
+# async def clear(ctx,lim=1) -> None:
+	# '''*(WIP)* Clears all error messages from this bot'''
+	# dlist = []
+	# async for message in client.logs_from(ctx.message.channel,limit=int(lim)):
+		# dlist.append(message)
+	# await client.delete_messages(dlist)
 
 @client.command()
 async def ping():
 	await client.say("Pong!")
-# @client.command(pass_context=True)
-# async def rdelete(ctx):
-	# '''Use reactions to mass delete messages'''
-	# #:regional_indicator_t:
-	# msg = ctx.message
-	# author = msg.author
-	# if (author.server_permissions.manage_messages):
-		# pass
-		
-		# # await client.send_message(channel, react)
-		# # logs = []
-		# # async for message in client.logs_from(channel, limit = 50, before=msg):
-			# # logs.append(message)		
-	# else:
-		# await client.say("Sorry, you don't have permissions for that!")	
 		
 @client.event
 async def on_reaction_add(reaction, user):
