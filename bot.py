dcec0a1a20e17fc3cf86b8c37313caeb2fa4fcd4
Justin Tran <MMusto@users.noreply.github.com>
2018-11-11 16:40:16 -0800

    Music Test 365

--- bot.py
+++ bot.py
@@ -2,7 +2,30 @@ import discord
 from discord.ext import commands
 import os
 import youtube_dl
+from discord import opus
+OPUS_LIBS = ['libopus-0.x86.dll', 'libopus-0.x64.dll',
+             'libopus-0.dll', 'libopus.so.0', 'libopus.0.dylib']
 
+
+def load_opus_lib(opus_libs=OPUS_LIBS):
+    if opus.is_loaded():
+        return True
+
+    for opus_lib in opus_libs:
+            try:
+                opus.load_opus(opus_lib)
+                return
+            except OSError:
+                pass
+
+    raise RuntimeError('Could not load an opus lib. Tried %s' %
+                       (', '.join(opus_libs)))
+opts = {
+    'default_search': 'auto',
+    'quiet': True,
+}  # youtube_dl options
+
+load_opus_lib()
 players = {}
 
 TOKEN = os.environ["TOKEN"]
