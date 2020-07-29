dfa0f61e8dc8e4b2e85c626f7f2d7b12bdf5a028
Justin Tran <MMusto@users.noreply.github.com>
2020-07-28 23:07:10 -0700

    Rename sounds
    Python script that renames '.mp3' files to have them appear last when sorted by name.

--- rename_sounds.py
+++
@@ -1,8 +0,0 @@
-import os
-from os import listdir
-from os.path import isfile, join
-mypath = "./"
-onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f)) and f[-3:] == "mp3"]
-
-for file in onlyfiles:
-    os.rename(file, 'Ω' + file)
