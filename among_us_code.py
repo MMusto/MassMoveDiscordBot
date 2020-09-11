35d18bbe1d61bf66f0c48ff5e06d35de5f1fee75
Justin Tran <MMusto@users.noreply.github.com>
2020-09-10 23:46:09 -0700

    Update among_us_code.py

--- among_us_code.py
+++ among_us_code.py
@@ -15,24 +15,24 @@ class AmongUs(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         #Taken from https://stackoverflow.com/questions/62503897/how-can-i-limit-the-on-message-replies-discord-python-bot/62504583#62504583
-        now = datetime.datetime.utcnow()
-        time_difference = (now - self.last_timeStamp).total_seconds()
-        code = message.content.strip()
         channel = message.channel
-        author = message.author
-        if time_difference > 5 and channel.id == CODE_CHANNEL_ID and self.bot.user != author and self.valid_code(code):
-                await channel.purge(limit=None)
-                await channel.send(f"Among Us Room Code: **{code.upper()}**")
-                print(f"[SUCCESS - {(now.hour - 8) % 24}:{now.minute}] Code '{code}' was set by {author.display_name} / {author.name}")
-                self.last_timeStamp = datetime.datetime.utcnow()
-        else:
-            
-            print(f"[FAILED - {(now.hour - 8) % 24}:{now.minute}] Code TRIED to be set to '{code}' by {author.display_name} / {author.name}")
-            message.delete()
+        if channel.id == CODE_CHANNEL_ID:
+            now = datetime.datetime.utcnow()
+            time_difference = (now - self.last_timeStamp).total_seconds()
+            code = message.content.strip()
+            author = message.author
+            if time_difference > 5  and self.bot.user != author and self.valid_code(code):
+                    await channel.purge(limit=None)
+                    await channel.send(f"Among Us Room Code: **{code.upper()}**")
+                    print(f"[SUCCESS - {(now.hour - 8) % 24}:{now.minute}] Code '{code}' was set by {author.display_name} / {author.name}")
+                    self.last_timeStamp = datetime.datetime.utcnow()
+            else:
+                print(f"[FAILED - {(now.hour - 8) % 24}:{now.minute}] Code TRIED to be set to '{code}' by {author.display_name} / {author.name}")
+                await message.delete()
                 
     @commands.command(hidden = True)
     async def testembed(self, ctx):
-        embed = discord.Embed(title = f"**AMONG US CODE**")
-        embed.add_field(name = "**TEST**", value = "   ", inline = False)
+        embed = discord.Embed(title = f"**AMONG US**")
+        embed.add_field(name = "ROOM CODE", value = "**TEST**", inline = False)
         embed.timestamp = ctx.message.created_at
         ctx.send(embed=embed)
