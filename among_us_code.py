6ac642a4eeb214e2c9207774f5f67d482e889241
Justin Tran <MMusto@users.noreply.github.com>
2020-09-10 20:54:32 -0700

    Update among_us_code.py

--- among_us_code.py
+++ among_us_code.py
@@ -2,14 +2,14 @@ import asyncio
 import discord
 from discord.ext import commands
 
-CODE_CHANNEL_ID = 753818860100255884
+CODE_CHANNEL_ID = 753825645032767559
 class AmongUs(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.channel.id == CODE_CHANNEL_ID:
+        if message.channel.id == CODE_CHANNEL_ID and self.bot.user != message.author:
             channel = message.channel
             code = message.content
             await channel.purge(limit=None)
