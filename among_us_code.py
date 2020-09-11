281ef785e92b249415363adb8a0195b0f5f09cc4
Justin Tran <MMusto@users.noreply.github.com>
2020-09-10 20:44:12 -0700

    Update among_us_code.py

--- among_us_code.py
+++ among_us_code.py
@@ -4,6 +4,9 @@ from discord.ext import commands
 
 CODE_CHANNEL_ID = 753818860100255884
 class AmongUs(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.channel.id == CODE_CHANNEL_ID:
