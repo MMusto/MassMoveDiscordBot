9effb49a4a5484c44a8176840503c978bb9b8e6b
Justin Tran <MMusto@users.noreply.github.com>
2020-09-17 19:31:16 -0700

    Update among_us_code.py

--- among_us_code.py
+++ among_us_code.py
@@ -3,6 +3,7 @@ import discord
 from discord.ext import commands
 import datetime
 
+COOLDOWN = 5
 CODE_CHANNEL_ID = 753825645032767559
 class AmongUs(commands.Cog):
     def __init__(self, bot):
@@ -10,7 +11,7 @@ class AmongUs(commands.Cog):
         self.last_timeStamp = datetime.datetime.utcfromtimestamp(0)
 
     def valid_code(self, code : str):
-        return  code.isalpha() and len(code) == 6
+        return code.isalpha() and len(code) == 6
 
     def get_embed(self, code):
         embed = discord.Embed(title = "Room Code:", color = 0x00f715)
@@ -26,7 +27,7 @@ class AmongUs(commands.Cog):
             time_difference = (now - self.last_timeStamp).total_seconds()
             code = message.content.strip()
             author = message.author
-            if time_difference > 5  and self.bot.user != author and self.valid_code(code):
+            if time_difference > COOLDOWN  and self.bot.user != author and self.valid_code(code):
                     await channel.purge(limit=None)
                     await channel.send(embed = self.get_embed(code.upper()))
                     print(f"[SUCCESS - {(now.hour - 8) % 24}:{now.minute}] Code '{code}' was set by {author.display_name} / {author.name}")
