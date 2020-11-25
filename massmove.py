35e59eead20505a6541c47bd2362091fb6fe76d9
MMusto <jhtran3@uci.edu>
2020-11-25 15:31:47 -0800

    .

--- massmove.py
+++ massmove.py
@@ -33,7 +33,7 @@ class MassMove(commands.Cog):
                     if before.id == cid:
                         msg = await self.control_panel.fetch_message(mid)
                         if msg:
-                            await msg.edit(f'**{after.name}**')
+                            await msg.edit(content = f'**{after.name}**')
                         else:
                             self.error('Control panel message not found!')
                         return
